64c43f51578217a1b76a7ab05ece6763aa51f45a
sage <laymonage@gmail.com>
2023-05-26 16:25:43 +0000

    Add function to get GraphQL host URL

---
+++ frontend/lib/graphql.ts
@@ -0,0 +1,8 @@
+export function getGraphQLHost() {
+  const gitpodUrl = new URL(process.env.GITPOD_WORKSPACE_URL as string);
+  const port = 8080;
+  const path = '/api/graphql/'
+  gitpodUrl.hostname = `${port}-${gitpodUrl.hostname}`;
+  gitpodUrl.pathname = path;
+  return gitpodUrl.toString();
+}
