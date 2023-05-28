9b7640753c3d8e87716e45a48e634d65d3efed38
sage <laymonage@gmail.com>
2023-05-28 14:34:29 +0000

    Use named query for home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,11 +3,11 @@ import { client, getHostOrigin } from '@/lib/graphql';
 
 export default async function Home() {
   const {
-    data: { page },
+    data: { homePage },
   } = await client.query({
     query: gql`
       query FetchHomePage {
-        page(urlPath: "/") {
+        homePage: page(urlPath: "/") {
           title
           ... on HomePage {
             heroText
@@ -24,14 +24,14 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <h1 className="text-4xl font-bold mb-8">{page.title}</h1>
+        <h1 className="text-4xl font-bold mb-8">{homePage.title}</h1>
       </div>
-      <p className="mb-8">{page.heroText}</p>
+      <p className="mb-8">{homePage.heroText}</p>
       <a
         className="px-4 py-2 rounded border hover:text-blue-100"
-        href={getHostOrigin() + page.heroCtaLink.url}
+        href={getHostOrigin() + homePage.heroCtaLink.url}
       >
-        {page.heroCta}
+        {homePage.heroCta}
       </a>
     </main>
   );
