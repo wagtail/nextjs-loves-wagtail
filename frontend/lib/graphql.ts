531064222f200c43eedc8665ee77c2f1aef57465
sage <laymonage@gmail.com>
2023-05-28 13:28:12 +0000

    Add typescript-eslint and prettier config

--- frontend/lib/graphql.ts
+++ frontend/lib/graphql.ts
@@ -1,7 +1,7 @@
 export function getGraphQLHost() {
   const gitpodUrl = new URL(process.env.GITPOD_WORKSPACE_URL as string);
   const port = 8080;
-  const path = '/api/graphql/'
+  const path = '/api/graphql/';
   gitpodUrl.hostname = `${port}-${gitpodUrl.hostname}`;
   gitpodUrl.pathname = path;
   return gitpodUrl.toString();
