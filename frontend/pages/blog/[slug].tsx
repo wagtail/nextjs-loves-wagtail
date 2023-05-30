f06f6f939e3bd5b6fa68f67b092397ef83eb8d89
Sage Abdullah <me@laymonage.com>
2023-05-30 21:02:51 +0100

    Fix build failure due to type mismatch

--- frontend/pages/blog/[slug].tsx
+++ frontend/pages/blog/[slug].tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 
 interface BlogPage {
@@ -32,8 +32,8 @@ const BlogArticle: NextPage<{ post: BlogPage }> = ({ post }) => {
 export default BlogArticle;
 
 // This function gets called at build time
-export async function getStaticProps({ params }) {
-  const { slug } = params;
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const slug = params?.slug as string;
   const data = await fetch(
     `http://localhost:8000/api/v2/pages/?${new URLSearchParams({
       slug,
@@ -47,22 +47,14 @@ export async function getStaticProps({ params }) {
     }
   ).then((response) => response.json());
 
-  if (!data.items.length) {
-    return {
-      props: {
-        post: null,
-      },
-    };
-  }
-
   return {
     props: {
       post: data.items[0],
     },
   };
-}
+};
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const data = await fetch(
     `http://localhost:8000/api/v2/pages/?${new URLSearchParams({
       type: "blog.BlogPage",
@@ -74,12 +66,14 @@ export async function getStaticPaths() {
     }
   ).then((response) => response.json());
 
+  const paths = data.items.map((post: BlogPage) => ({
+    params: {
+      slug: post.meta.slug,
+    },
+  }));
+
   return {
-    paths: data.items.map((post: BlogPage) => ({
-      params: {
-        slug: post.meta.slug,
-      },
-    })),
-    fallback: true,
+    paths: paths,
+    fallback: false,
   };
-}
+};
