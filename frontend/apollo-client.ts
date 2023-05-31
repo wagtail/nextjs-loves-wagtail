e5ab4f8a428e6a32506ebfce80eb2084c3fb88e2
Sage Abdullah <me@laymonage.com>
2023-05-31 07:54:56 +0100

    Delete frontend

--- frontend/apollo-client.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://bakerydemo-thibaudcolas5.herokuapp.com/api/graphql/",
-  cache: new InMemoryCache(),
-});
-
-export default client;
